bd2bb46b8a7edd4de49f5b4a93959cdf8e53af64
ste7022424 <ste7022424@gmail.com>
2018-03-22 11:13:31 +0800

    Test1
    111

---
+++ 1_Class/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var S1 = new School_Member("小張",10,2);
+            Console.WriteLine(S1.AboutYourSelf());
+        }
+    }
+}
